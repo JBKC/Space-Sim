f28d5630760886e3c7a145155117315e6814bb9f
jbkc <jmbannon97@gmail.com>
2025-03-26 03:12:52 +0000

    Update all Three.js and related imports to use CDN URLs

--- src/moon3D.js
+++ src/moon3D.js
@@ -1,9 +1,9 @@
 // IMPORTS
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/0.174.0/three.module.js';
-import { TilesRenderer } from '3d-tiles-renderer/src/three/TilesRenderer.js';
-import { CesiumIonAuthPlugin } from '3d-tiles-renderer/src/plugins/three/CesiumIonAuthPlugin.js';
-import { GLTFExtensionsPlugin } from '3d-tiles-renderer/src/plugins/three/GLTFExtensionsPlugin.js';
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
+import { TilesRenderer } from 'https://unpkg.com/3d-tiles-renderer@0.3.20/src/three/TilesRenderer.js';
+import { CesiumIonAuthPlugin } from 'https://unpkg.com/3d-tiles-renderer@0.3.20/src/plugins/three/CesiumIonAuthPlugin.js';
+import { GLTFExtensionsPlugin } from 'https://unpkg.com/3d-tiles-renderer@0.3.20/src/plugins/three/GLTFExtensionsPlugin.js';
+import { DRACOLoader } from 'https://unpkg.com/three@0.174.0/examples/jsm/loaders/DRACOLoader.js';
 import { createSpacecraft } from './spacecraft.js';
 import { fireLaser, updateLasers } from './laser.js';
 import { 
